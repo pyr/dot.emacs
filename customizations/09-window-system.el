c568471780acf9d50f523dbac94b3bc3606e6fe2
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-04-21 11:42:41 +0200

    conditional font

--- customizations/09-window-system.el
+++ customizations/09-window-system.el
@@ -1,10 +1,18 @@
 (defun set-default-font (frame)
   (when window-system
-    (set-face-attribute 'default nil :font "Inconsolata 12")))
+    (cond
+      ((string-match "apple-darwin" system-configuration)
+         (set-face-attribute 'default nil :font "Inconsolata 12"))
+      ((string-match "linux" system-configuration)
+         (set-face-attribute 'default nil :font "Terminus 12")))))
 
-(setq default-frame-alist '((font-backend . "xft")
-			    (font . "Inconsolata 12")))
+(cond
+  ((string-match "apple-darwin" system-configuration)
+     (setq default-frame-alist '((font-backend . "xft")
+                                 (font . "Inconsolata 12"))))
+  ((string-match "linux" system-configuration)
+     (setq default-frame-alist '((font-backend . "xft")
+                                 (font . "Terminus 12")))))
 
 (set-default-font nil)
-
 (add-hook 'after-make-frame-functions 'set-default-font)
