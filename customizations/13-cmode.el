fe18700648b7701c01be56a296c222f08ce8e238
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-07-01 16:36:55 +0200

    change cmode

--- customizations/13-cmode.el
+++ customizations/13-cmode.el
@@ -2,8 +2,16 @@
 ;; Write C in BSD style
 ;;
 (setq c-default-style '(("c-mode" . "k&r")))
-(setq c-basic-offset 8)
-(setq-default tab-wdith 8)
+;;(setq c-basic-offset 8)
+;;(setq-default tab-wdith 8)
 (add-hook 'c-mode-common-hook
              (lambda ()
-               (setq indent-tabs-mode t)))
+               (setq c-basic-offset 4
+                     tab-width 4
+                     indent-tabs-mode nil)))
+
+(add-hook 'java-mode-hook
+                (lambda ()
+                  (setq c-basic-offset 4
+                        tab-width 4
+                        indent-tabs-mode nil)))
