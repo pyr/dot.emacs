9e3b8b644cf5ac7b44aff590a24417197d3c7742
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-11-12 10:08:31 +0100

    better fonts on mac

--- customizations/39-system-tweaks.el
+++ customizations/39-system-tweaks.el
@@ -1,4 +1,4 @@
 (when (and window-system (string-match "apple-darwin" system-configuration))
-  (setq graphene-default-font "Inconsolata 12")
-  (setq graphene-fixed-pitch-font "Inconsolata 12")
-  (set-face-attribute 'default nil :font "Inconsolata 12"))
+  (setq graphene-default-font "Inconsolata 14")
+  (setq graphene-fixed-pitch-font "Inconsolata 14")
+  (set-face-attribute 'default nil :font "Inconsolata 14"))
