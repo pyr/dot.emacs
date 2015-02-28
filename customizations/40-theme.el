20f06ff6345f1c76d30f9ac1aed54ddf0e3b9531
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-02-28 10:39:51 +0100

    update solarized theme

--- customizations/40-theme.el
+++ customizations/40-theme.el
@@ -4,21 +4,26 @@
 
 (custom-set-variables '(solarized-termcolors 256))
 
+(setq solarized-default-background-mode 'dark)
+
 (load-theme 'solarized t)
 
-(defun set-background-mode (mode)
-  (set-frame-parameter nil 'background-mode mode)
-  (when (not (display-graphic-p (selected-frame)))
-    (set-terminal-parameter nil 'background-mode mode))
+(defun set-background-mode (frame mode)
+  (set-frame-parameter frame 'background-mode mode)
+  (when (not (display-graphic-p frame))
+    (set-terminal-parameter (frame-terminal frame) 'background-mode mode))
   (enable-theme 'solarized))
 
 (defun switch-theme ()
   (interactive)
   (let ((mode  (if (eq (frame-parameter nil 'background-mode) 'dark)
                    'light 'dark)))
-    (set-background-mode mode)))
+    (set-background-mode nil mode)))
 
-(set-background-mode 'dark)
+(add-hook 'after-make-frame-functions
+          (lambda (frame) (set-background-mode frame solarized-default-background-mode)))
+
+(set-background-mode nil solarized-default-background-mode)
 
 (global-set-key (kbd "C-c t") 'switch-theme)
 
