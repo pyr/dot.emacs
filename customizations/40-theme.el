f14214823548176c946b61f2706ef878bdfacc61
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-02-28 10:14:48 +0100

    update solarized theme

--- customizations/40-theme.el
+++ customizations/40-theme.el
@@ -1,34 +1,25 @@
 ;;
 ;; Globally map C-c t to a light/dark theme switcher
 ;; Also pull-in graphene for better fonts
-;;(require 'solarized)
+
+(custom-set-variables '(solarized-termcolors 256))
 
 (load-theme 'solarized t)
 
-(defun get-frame ()
-  (window-frame (frame-first-window)))
+(defun set-background-mode (mode)
+  (set-frame-parameter nil 'background-mode mode)
+  (when (not (display-graphic-p (selected-frame)))
+    (set-terminal-parameter nil 'background-mode mode))
+  (enable-theme 'solarized))
 
 (defun switch-theme ()
   (interactive)
-  (let* ((bmode (frame-parameter (get-frame) 'background-mode))
-         (mode  (if (eq bmode 'light) 'dark 'light)))
-    (message (format "switching frame background-mode to: %s"
-                     (symbol-name mode)))
-    (set-frame-parameter (get-frame)
-                         'background-mode
-                         mode)
-    (enable-theme 'solarized)))
+  (let ((mode  (if (eq (frame-parameter nil 'background-mode) 'dark)
+                   'light 'dark)))
+    (set-background-mode mode)))
 
-(custom-set-variables
-  '(custom-safe-themes
-    (quote
-     ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6"
-      "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365"
-      default))))
+(set-background-mode 'dark)
 
 (global-set-key (kbd "C-c t") 'switch-theme)
-
-;;(when window-system
-;;  (load-theme 'solarized-light))
 
 (require 'graphene)
