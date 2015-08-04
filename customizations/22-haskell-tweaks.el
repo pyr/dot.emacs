050d88fdeee08c0eba9a9d37991c4f1698d7c8b8
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-08-04 20:30:25 +0200

    haskell: adapt to new haskell-mode

--- customizations/22-haskell-tweaks.el
+++ customizations/22-haskell-tweaks.el
@@ -3,5 +3,12 @@
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
 (setq ghc-ghc-options '("-XDeriveGeneric"))
-(setq haskell-program-name "cabal repl ")
+
+(custom-set-variables
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-type 'cabal-repl))
