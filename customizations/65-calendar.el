6ad22317f4ae6d0de451606eddcb9eaaa7a21fb4
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-01-26 11:59:18 +0100

    calendar config with external credentials.

---
+++ customizations/65-calendar.el
@@ -0,0 +1,16 @@
+(require 'calfw)
+(require 'calfw-org)
+(require 'org-gcal)
+
+(let* ((path          "~/.emacs.d/.gcal-credentials")
+       (calendar-path "~/Documents/Dropbox/Perso/Org/main-calendar.org")
+       (creds         (with-temp-buffer
+                        (insert-file-contents-literally path)
+                        (goto-char 0)
+                        (read (buffer-string))))
+       (client-id     (car creds))
+       (client-secret (car (cdr creds))))
+  (setq org-gcal-client-id     client-id
+        org-gcal-client-secret client-secret
+        org-gcal-file-alist    `(("pyr@spootnik.org" . ,calendar-path))
+        org-agenda-files       `(,calendar-path)))
