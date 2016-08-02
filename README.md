776c0e66199f56c5dcce0d85b8c6162b0e5542be
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-08-02 23:08:41 +0200

    switch to emenv

--- README.md
+++ README.md
@@ -17,8 +17,8 @@ You will want a recent version of emacs installed:
 - archlinux users will have access to emacs 24.3 through pacman
 - MacOS users will want to install from: http://emacsformacosx.com/
 
-This configuration relies on [cask](http://cask.github.io), please refer to
-http://cask.github.io/installation to get cask installed.
+This configuration relies on [emenv](https://github.com/pyr/emenv), please install
+it.
 
 Once cask is set-up you can just clone this directory in your
 `$HOME/.emacs.d`:
@@ -30,12 +30,12 @@ cd $HOME && git clone https://github.com/pyr/dot.emacs .emacs.d
 You can then install dependencies by running (within `$HOME/.emacs.d`):
 
 ```
-cask install
+emenv install
 ```
 
 ### Curating your list of packages
 
-The `$HOME/.emacs.d/Cask` file contains the list of dependencies you
+The `$HOME/.emacs.d/Emenv` file contains the list of dependencies you
 wish to pull in. You will most likely want to fork this repository and
 maintain your own list of dependencies, since this it will be
 dependent on your workflow and personal preference.
@@ -63,10 +63,4 @@ Some notable out of the box customizations:
 
 ### Updating dependencies
 
-Dependencies may be updated by running `cask update`. Cask itself
-may be updated by running `cask upgrade`.
-
-## Recent local changes
-
-- haskell-mode needs unquoting: https://github.com/haskell/haskell-mode/issues/596
-- o-blog v1 needs to switch to org-export-as: 
+Dependencies may be updated by running `cask sync` then `cask install`.
--- init.el
+++ init.el
@@ -8,13 +8,4 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "ebd976c3d91de7c6858db3ba07c52238d545a106c8fbf05192913f6d42421621" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
- '(quack-programs
-   (quote
-    ("csi -:c" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "ebd976c3d91de7c6858db3ba07c52238d545a106c8fbf05192913f6d42421621" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
