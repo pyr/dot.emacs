ad1085ee21abfc793c32a7ace0d86554bf8194c3
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-03-12 10:48:52 +0100

    org repo unneeded

--- packages.el
+++ packages.el
@@ -6,8 +6,8 @@
   '(("ELPA"         . "http://tromey.com/elpa/")
     ("gnu"          . "http://elpa.gnu.org/packages/")
     ("melpa"        . "http://melpa.org/packages/")
-    ("melpa-stable" . "http://stable.melpa.org/packages/")
-    ("org"          . "http://orgmode.org/elpa/")))
+    ("melpa-stable" . "http://stable.melpa.org/packages/")))
+;;    ("org"          . "http://orgmode.org/elpa/")))
 
 (setq package-pinned-packages
   '((cider . "melpa-stable")
